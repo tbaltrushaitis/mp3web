8eae515b6a3da81a508c2117b2700624635caac3
Tomas B <tbaltrushaitis@gmail.com>
2016-09-11 06:24:37 +0300

    HTML Document sections templates - head, footer, javascript includes

---
+++ src/resources/views/doc/head.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--[if IE 8]> <html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml"> <!--<![endif]-->
+<head>
+    <!--/ Basic Page Needs
+    ================================================== /-->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <title>Premium Music Collection</title>
+
+    <!--/ Metas
+    ================================================== /-->
+    <meta name="title"          content="Premium Music Collection" />
+    <meta name="description"    content="MP3 Audio DataBase" />
+    <meta name="keywords"       content="mp3, player, laravel" />
+    <meta name="author"         content="tbaltrushaitis@gmail.com" />
+    <meta name="language"       content="en" />
+    <meta name="robots"         content="index, follow, noarchive" />
+
+    <!--/ Application Specific Metas
+    ================================================== /-->
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+    <!--/ Metas for Crawlers
+    ================================================== /-->
+    <meta property="og:type"        content="collection" />
+    <meta property="og:title"       content="Premium Music Collection" />
+    <meta property="og:description" content="MP3 Audio DataBase" />
+    <meta property="og:image"       content="assets/img/logo/logo-lg.png" />
+    <meta property="og:site_name"   content="mp3 Manager and Player" />
+    <meta property="og:url"         content="{{ url('/') }}" />
+    <meta property="og:see_also"    content="https://github.com/tbaltrushaitis/mp3" />
+
+    <!--/ Mobile Specific Metas
+    ================================================== /-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+
+    <!--/ Fonts
+    ================================================== /-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" />
+
+    <!--/ Styles
+    ================================================== /-->
+    <!--/ bootstrap /-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" />
+    <!--/ Custom /-->
+    <link rel="stylesheet" href="assets/css/fonts-bundle.css" />
+    <link rel="stylesheet" href="assets/css/plugins-bundle.css" />
+    <!--/ <link rel="stylesheet" href="assets/js/plugins/raty/jquery.raty.css" /> /-->
+    <link rel="stylesheet" href="assets/css/styles.css" id="css-default" />
+
+    <!--/ Favicon
+    ================================================== /-->
+    <link href="assets/img/favicon.ico" type="image/x-icon" rel="icon" />
+    <link href="assets/img/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+
+    <!--/ HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries /-->
+    <!--[if lt IE 9]>
+        <script src="assets/js/plugins/misc/html5shiv.min.js"; ?>"></script>
+        <script src="assets/js/plugins/misc/respond.min.js"; ?>"></script>
+    <![endif]-->
+</head>
