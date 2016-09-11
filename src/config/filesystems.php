ebcd5fb643c226ec98fa578fedc6ac4b363fc417
Tomas B <tbaltrushaitis@gmail.com>
2016-09-11 06:16:31 +0300

    Configuration of virtual storages for audio, video and metadata files

---
+++ src/config/filesystems.php
@@ -0,0 +1,71 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filesystem Disk
+    |--------------------------------------------------------------------------
+    */
+
+    'default' => 'local'
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Cloud Filesystem Disk
+    |--------------------------------------------------------------------------
+    */
+
+  , 'cloud' => 's3'
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filesystem Disks
+    |--------------------------------------------------------------------------
+    */
+
+  , 'disks' => [
+
+        'local' => [
+            'driver' => 'local'
+          , 'root'   => storage_path('app')
+        ]
+
+      , 'public' => [
+            'driver'     => 'local'
+          , 'root'       => storage_path('app/public')
+          , 'visibility' => 'public'
+        ]
+
+        /*
+        |----------------------------------------------------------------------
+        | Media Storage
+        |----------------------------------------------------------------------
+        */
+      , 'media' => [
+            'driver'     => 'local'
+          , 'root'       => storage_path('media')
+          , 'visibility' => 'public'
+        ]
+
+      , 'audio' => [
+            'driver'     => 'local'
+          , 'root'       => storage_path('media/audio')
+          , 'visibility' => 'public'
+        ]
+
+      , 'video' => [
+            'driver'     => 'local'
+          , 'root'       => storage_path('media/video')
+          , 'visibility' => 'public'
+        ]
+
+      , 'meta' => [
+            'driver'     => 'local'
+          , 'root'       => storage_path('app/metadata')
+          , 'visibility' => 'public'
+        ]
+
+    ]
+
+];
