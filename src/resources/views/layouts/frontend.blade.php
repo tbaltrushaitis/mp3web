a5c73583df4a5d9a171e69c9c4dfb830ed0d1f5f
Tomas B <tbaltrushaitis@gmail.com>
2016-09-13 14:39:43 +0300

    small changes

--- src/resources/views/layouts/frontend.blade.php
+++ src/resources/views/layouts/frontend.blade.php
@@ -1,6 +1,6 @@
 @include('doc.head')
 
-<body id="homepage" class="homepage skin-default theme-default" data-id_page="layout-frontend" data-rooturl="{{ Request::root() }}">
+<body id="layout-frontend" class="homepage skin-default theme-default" data-id_page="layout-frontend" data-rooturl="{{ Request::root() }}">
 
     <!--/ START: NavBar-Top /-->
     <header class="header" id="header">
@@ -25,8 +25,8 @@
                     <ul class="nav navbar-nav ">
                         <!--/ Authentication Links /-->
                         @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            {{-- <li><a href="{{ url('/register') }}">Register</a></li> --}}
+                            <li><a href="{{ url('/login') }}"> Login</a></li>
+                            {{-- <li><a href="{{ url('/register') }}"> Register</a></li> --}}
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -34,7 +34,8 @@
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
-                                    <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                    <li><a href="{{ url('/home') }}" target="_blank"><i class="fa fa-btn fa-dashboard"></i>  Cabinet</a></li>
+                                    <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>  Logout</a></li>
                                 </ul>
                             </li>
                         @endif
