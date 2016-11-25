858491b344602a0a15131b4b1c14f631fd8de918
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2016-11-25 09:19:17 +0200

    Removed lines previously commented

--- src/resources/views/doc/javascripts.blade.php
+++ src/resources/views/doc/javascripts.blade.php
@@ -1,7 +1,5 @@
 <!--/ JavaScripts
 ================================================== /-->
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script> --}}
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script> --}}
 
 <!--/ REQUIRED /-->
 <script src="{{ asset('/assets/js/lib/require.js') }}" data-main="{{ asset('/assets/js/app-config-require.js') }}"></script>
