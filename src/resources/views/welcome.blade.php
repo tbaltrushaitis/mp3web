4f27facbf7c7e3e960caf342a8b5a99639e99f93
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2016-11-28 06:12:52 +0200

    div-wrapper around tagsInput block

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -25,8 +25,10 @@
                 @else
                     <span class="label label-success">{{ $track->get('plays', 0) }}</span>
                 @endif
-                <a href="{{ $track->get('id') }}">{{ $track->get('title') }}</a>
-                <input class="genre" value="{{ $track->get('genre') }}" data-role="tagsinput" />
+                <a class="" href="{{ $track->get('id') }}">{{ $track->get('title') }}</a>
+                <div class="bootstrap-tagsinput">
+                    <input class="genre" value="{{ $track->get('genre') }}" data-role="tagsinput" />
+                </div>
                 <div class="raty" data-score="{{ ($track->get('likes') * 5) / max( $track->get('likes') + $track->get('dislikes'), 1) }}"></div>
             </li>
         @endforeach
