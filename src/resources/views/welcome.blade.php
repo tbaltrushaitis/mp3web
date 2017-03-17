15a37c0256c3cdbc1f8582e6238b02b8269b50ba
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2017-03-17 15:04:37 +0200

    Track labels, tags and raty moved to right side of item's layout

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -20,20 +20,23 @@
         @foreach ($tracks as $idx => $track)
             <li class="list-group-item track">
                 <i class="fa fa-headphones fa-fw"></i>
-                @if ((time() - $track->get('added')) < 604800)
+                @if ((time() - $track->get('added')) < 60 * 60 * 24 * 14)
                     <span class="label label-danger">New!</span>
                 @else
                     <span class="label label-success">{{ $track->get('plays', 0) }}</span>
                 @endif
                 <a class="" href="{{ $track->get('id') }}">{{ $track->get('title') }}</a>
 
-                @if (is_array($track->get('genre')) && count($track->get('genre')) > 0)
-                    @foreach ( $track->get('genre') as $idx => $Genre )
-                        <span class="label label-primary tagholder" id="tag_genre_{{$idx}}">{{ $Genre }}</span>
-                    @endforeach
-                @endif
+                <div class="raty pull-right" data-score="{{ ($track->get('likes') * 5) / max( $track->get('likes') + $track->get('dislikes'), 1) }}"></div>
 
-                <div class="raty" data-score="{{ ($track->get('likes') * 5) / max( $track->get('likes') + $track->get('dislikes'), 1) }}"></div>
+                <div class="labels-holder pull-right">
+                    @if (is_array($track->get('genre')) && count($track->get('genre')) > 0)
+                        @foreach ( $track->get('genre') as $idx => $Genre )
+                            <span class="label label-primary tagholder" id="tag_genre_{{$idx}}">{{ $Genre }}</span>
+                        @endforeach
+                    @endif
+                </div>
+
             </li>
         @endforeach
     @else
