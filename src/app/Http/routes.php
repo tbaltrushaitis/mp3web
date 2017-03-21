2199d520cedea872199db57725a0b9539e212f16
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2017-03-21 04:27:10 +0200

    new(route): /{hash}/play

--- src/app/Http/routes.php
+++ src/app/Http/routes.php
@@ -1,12 +1,33 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------------
 |   Application Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------------
 */
-Route::get('/',         ['as' => 'Front:Index',     'uses' => 'MediaController@listAudio']);
+Route::get('/', ['as' => 'Front:Index', 'uses' => 'MediaController@listAudio']);
 
+
+/*
+|-------------------------------------------------------------------------------
+|   Tracks Properties manipulation Routes
+|-------------------------------------------------------------------------------
+*/
+Route::get('/{hash}',               ['as' => 'Media:Play',  'uses' => 'MediaController@mediaPlay'])
+    ->where(['hash' => '[0-9a-f]+']);
+
+Route::get('/{hash}/play',          ['as' => 'Media:Play',  'uses' => 'MediaController@mediaPlay'])
+    ->where(['hash' => '[0-9a-f]+']);
+
+Route::post('rate/{hash}/{rate}',   ['as' => 'Media:Rate',  'uses' => 'MediaController@mediaRate'])
+    ->where(['hash' => '[0-9a-f]+', 'rate' => '(dis)?like']);
+
+
+/*
+|-------------------------------------------------------------------------------
+|   Backend Routes
+|-------------------------------------------------------------------------------
+*/
 Route::get('login',     ['as' => 'Auth:LoginForm',  'uses' => 'Auth\AuthController@showLoginForm']);
 Route::post('login',    ['as' => 'Auth:Login',      'uses' => 'Auth\AuthController@login']);
 Route::get('logout',    ['as' => 'Auth:Logout',     'uses' => 'Auth\AuthController@logout']);
@@ -18,32 +39,22 @@ Route::get('password/reset/{token?}',   'Auth\PasswordController@showResetForm')
 Route::get('register',  ['as' => 'Auth:RegistrationForm',   'uses' => 'Auth\AuthController@showRegistrationForm']);
 Route::post('register', ['as' => 'Auth:Register',           'uses' => 'Auth\AuthController@register']);
 
-/*
-|--------------------------------------------------------------------------
-|   Tracks Properties manipulation Routes
-|--------------------------------------------------------------------------
-*/
-Route::get('/{hash}',               ['as' => 'Media:Play',  'uses' => 'MediaController@mediaPlay'])
-    ->where(['hash' => '[0-9a-f]+']);
-
-Route::post('rate/{hash}/{rate}',   ['as' => 'Media:Rate',  'uses' => 'MediaController@mediaRate'])
-    ->where(['hash' => '[0-9a-f]+', 'rate' => '(dis)?like']);
 
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------------
 |   Cabinet Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------------
 */
 Route::get('/home', ['as' => 'Cabinet:Index', 'uses' => 'HomeController@index']);
 
+
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------------
 |   AJAX Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------------
 */
 Route::get('/{hash}/meta',  ['as' => 'Meta:get',    'uses' => 'AjaxController@getMeta'])
     ->where(['hash' => '[0-9a-f]+']);
-
 
 Route::post('/{hash}/meta', ['as' => 'Meta:update', 'uses' => 'AjaxController@updateMeta'])
     ->where(['hash' => '[0-9a-f]+']);
