371fb14c6186e46ec20be3ee11bceff3cccc175c
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2017-03-28 09:09:06 +0300

    Framework engine functions (laravel)

---
+++ bin/setup-engine.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+##  ------------------------------------------------------------------------  ##
+##  ENGINE
+##  ------------------------------------------------------------------------  ##
+
+function engine_setup {
+    composer -vvv create-project --prefer-dist ${ENGINE_NAME}/${ENGINE_TAG} "${ENGINE_DIR}" "${ENGINE_VERSION}.*"
+    cp -prv "${ENGINE_DIR}" build/ && cp -prv setup.rc build/.env && cd build && composer -vvv update && cd ..
+    printf "[INFO]\tEngine ${ENGINE_NAME}-${ENGINE_VERSION} deployed to ${ENGINE_DIR} \n";
+}
+
+
+function check_engine {
+    ENGINE_DIR="${ENGINE_NAME}-${ENGINE_VERSION}"
+    if [ ! -d "${ENGINE_DIR}" ]; then
+        printf "[WARNING]\tEngine directory ${ENGINE_DIR} not found!\n";
+        printf "[LOG]\tStarting engine setup ... \n";
+        engine_setup
+        # exit 1
+    fi
+}
