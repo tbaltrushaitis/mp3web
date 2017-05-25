548544f5356e20e70143a1ad5df7f822d8019729
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2017-05-25 04:15:20 +0300

    Now take parameters such as install, build, deploy, etc.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
+##  ------------------------------------------------------------------------  ##
+##                   Build project's working directory                        ##
+##  ------------------------------------------------------------------------  ##
+
+set -e
+trap 'echo >&2 Ctrl+C captured, exiting; exit 1' SIGINT
+
+function usage () {
+    >&2 cat << EOM
+            ${BWhite}Build application${NC}
+
+Usage: $0 <command> [<params>]
+
+    $0 usage                    -   show usage information
+    $0 <image | i> [<image_id>] -   build image
+
+EOM
+    RETVAL=1
+}
+
 #
 #   Script to initialize repo
 # - install required node packages
@@ -10,11 +30,23 @@ source setup.rc
 ##                                PREREQUISITES                               ##
 ##  ------------------------------------------------------------------------  ##
 
+OPTS=$@
+
+WD="$(cd $(dirname $0) && pwd -P)"   #   Current working directory
 APP_HOME="$(pwd)/"                      #   Current directory
 APP_PATH="${APP_HOME}${APP_DIR}"        #   Full path to target directory
 WEB_USER="www-data"                     #   Group of webserver used on host
 ENGINE_DIR="${ENGINE_NAME}-${ENGINE_VERSION}"
+CODE_VERSION="$(cat ./VERSION)"
 
+BUILD="${WD}/build"
+SRC="${WD}/src"
+DIST="${WD}/dist"
+
+DATE="$(date +"%Y%m%d%H%M%S")"
+DATETIME="$(date "+%Y-%m-%d")_$(date "+%H-%M-%S")"
+
+# printf "\n----------------------------  ${DATE}  ---------------------------\n";
 
 ##  ------------------------------------------------------------------------  ##
 ##                                PRE-CHECKS                                  ##
@@ -26,13 +58,24 @@ source bin/f-node.sh
 source bin/f-php-composer.sh
 source bin/host-checks.sh
 
-okNode
-okNpm
-okBower
-okGulp
 
-info "\tAPP_PATH=${APP_PATH}\n";
-info "\tENGINE_DIR=${ENGINE_DIR}\n";
+function preSetupChecks () {
+    info "\t$FUNCNAME params: \t [$@]\n";
+
+    okNode
+    okNpm
+    okBower
+    okGulp
+
+}
+
+info "WD = \t ${WD}";
+info "BUILD = \t ${BUILD}";
+info "SRC = \t ${SRC}";
+info "CODE_VERSION = \t ${CODE_VERSION}";
+info "APP_PATH = \t ${APP_PATH}";
+info "ENGINE_DIR = \t ${ENGINE_DIR}";
+
 
 ##  ------------------------------------------------------------------------  ##
 ##                                 GIT HOOKS                                  ##
@@ -60,40 +103,71 @@ info "\tENGINE_DIR=${ENGINE_DIR}\n";
 ##                                 EXECUTION                                  ##
 ##  ------------------------------------------------------------------------  ##
 
-check_composer
-sleep 1;
+function depsChecks () {
+    info "\t$FUNCNAME params: \t [$@]\n";
 
-check_engine
-sleep 1;
+    check_composer
+    sleep 1;
 
-fix_permissions
-sleep 1;
+    check_engine
+    sleep 1;
+
+    # fix_permissions
+    # sleep 1;
+
+    deps_install
+    sleep 1;
+}
 
 # check_git
 
 # git_update
 # sleep 1;
 
-deps_install
-sleep 1;
 # deps_outdated
 # sleep 1;
 
-# gulp build:dev --env=dev #--verbose
-gulp --env=${APP_ENV} #--verbose
-sleep 1;
 
-sudo chown -R ${WEB_USER}:${WEB_USER} build/
-sleep 1;
+function Build () {
+    info "\t$FUNCNAME params: \t [$@]\n";
 
-gulp sync:web --env=${APP_ENV} --verbose
-sleep 1;
+    cd ${WD}
+    gulp --env=${APP_ENV} #--verbose
+    sleep 1;
 
-sudo chown -R ${WEB_USER}:${WEB_USER} webroot/
-sleep 1;
+    cd ${WD}
+    cp -pr ./setup.rc "${BUILD}/.env.setup"
 
-gulp artisan:clear --env=${APP_ENV} --verbose
-sleep 1;
+    cd build/
+    composer -vvv update
+
+    sudo chown -R ${WEB_USER}:${WEB_USER} "${BUILD}/"
+    sleep 1;
+
+}
+
+
+function Deploy () {
+    printf "\t$FUNCNAME params: \t [$@]\n";
+
+    cd ${WD}
+    gulp sync:web --env=${APP_ENV} --verbose
+    sleep 1;
+
+    cd ${WD}
+    cd "${BUILD}/public/"
+    ln -s ../storage/media/audio/ 1>&2 2>/dev/null
+    sleep 1;
+
+    cd ${WD}
+    sudo chown -R "${WEB_USER}":"${WEB_USER}" "${APP_DIR}"
+    sleep 1;
+
+    gulp artisan:clear --env=${APP_ENV} --verbose
+    sleep 1;
+
+}
+
 
 # gulp deploy  --env=dev --verbose
 
@@ -103,5 +177,70 @@ sleep 1;
 # gulp dist
 # gulp deploy;
 
+##  ------------------------------------------------------------------------  ##
+##                                  EXECUTION                                 ##
+##  ------------------------------------------------------------------------  ##
+printf "\n-------------------------\t $0 $1 \t----------------------------\n";
+
+case "$1" in
+
+    "")
+        usage
+        RETVAL=1
+    ;;
+
+    "usage" | "help" | "-h")
+        splash "usage()";
+        usage
+        RETVAL=1
+    ;;
+
+    "test" | "-t")
+        splash "test()";
+        $0 "pre"
+        $0 "deps"
+        RETVAL=$?
+    ;;
+
+    "pre")
+        splash "pre()";
+        preSetupChecks
+        RETVAL=$?
+    ;;
+
+    "deps")
+        splash "deps()";
+        depsChecks
+        RETVAL=$?
+    ;;
+
+    "build" | "install" | "i" | "-b")
+        splash "Build()";
+        Build
+        RETVAL=$?
+    ;;
+
+    "rebuild" | "b" | "-b")
+        splash "REbuild()";
+        Build
+        Deploy
+        RETVAL=$?
+    ;;
+
+    "deploy")
+        splash "deploy()";
+        Deploy
+        RETVAL=$?
+    ;;
+
+    *)
+        RETVAL=1
+    ;;
+
+esac
+
 printf "\n\n[LOG]\tALL DONE\n"
+
+exit $RETVAL
+
 ##  --------------------------  EOF: setup.sh  -----------------------------  ##
