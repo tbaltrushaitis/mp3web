9ecb06b567b40b0b335c45f232a9d79aceb59555
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2017-11-16 11:30:46 +0200

    Setup script small pre-update

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@
 #   -   Deploy
 
 if [ -n "$APP_DEBUG" ]; then
-    set -x
+  set -x
 fi
 
 set -e
@@ -24,23 +24,24 @@ trap 'echo >&2 Ctrl+C captured, exiting; exit 1' SIGINT
 
 ##  Colors
 if [ -f ./bin/.bash_colors ]; then
-    source ./bin/.bash_colors
+  source ./bin/.bash_colors
+  echo -e "\t${BPurple}ENV:\t exported [./bin/.bash_colors]${NC}";
 fi
 
 ## Source settings
-if [ -f ./setup.rc ]; then
-    source setup.rc
-else
-    printf "Missing file [setup.rc]\n"
-    exit 1
+if [ ! -f ./setup.rc ]; then
+  printf "Missing file [setup.rc]\n"
+  exit 1
 fi
+source ./setup.rc
+echo -e "\t${BYellow}ENV:\t exported [./setup.rc]${NC}";
 
 
 function usage () {
-    >&2 cat << EOM
-            ${BWhite}Build application${NC}
+  >&2 cat << EOM
+            Build project ecosystem
 
-Usage:
+  Usage:
     $0 <command> [<params>]
 
     $0 <usage | help | h>   -   Show usage information
@@ -51,7 +52,7 @@ Usage:
     $0 <rebuild | rb>       -   Perform <build> and then <deploy> tasks
 
 EOM
-    RETVAL=1
+  RETVAL=1;
 }
 
 ##  root password for sudo operations
@@ -66,8 +67,9 @@ sudo -v
 
 OPTS=$@
 
-WD="$(cd $(dirname $0) && pwd -P)"      #   Current working directory
-APP_HOME="$(pwd)/"                      #   Current directory
+# WD="$(cd $(dirname $0) && pwd -P)"    #   Current working directory
+WD="$(pwd -P)"                          #   Current working directory
+APP_HOME="$(pwd -P)/"                   #   Current directory
 APP_PATH="${APP_HOME}${APP_DIR}"        #   Full path to target directory
 ENGINE_DIR="${ENGINE_NAME}-${ENGINE_VERSION}"
 
@@ -84,9 +86,9 @@ DATE="$(date +"%Y%m%d%H%M%S")"
 DATETIME="$(date "+%Y-%m-%d")_$(date "+%H-%M-%S")"
 
 for D in "${BUILD} ${DIST}"
-    do
-        mkdir -p "${D}"
-    done
+  do
+    mkdir -p "${D}"
+  done
 
 # printf "\n----------------------------  ${DATE}  ---------------------------\n";
 
@@ -95,26 +97,27 @@ for D in "${BUILD} ${DIST}"
 ##  ------------------------------------------------------------------------  ##
 
 source bin/f.sh
+source bin/f-database.sh
 source bin/f-engine.sh
 source bin/f-node.sh
 source bin/f-php-composer.sh
 source bin/host-checks.sh
 
 function logEnv () {
-    splash "$FUNCNAME Started with: (${@})";
+  splash "$FUNCNAME Started with: (${@})";
 
-    info "CODE_VERSION =  ${CODE_VERSION}";
-    info "GIT_COMMIT = \t ${GIT_COMMIT}";
-    info "WD = \t\t ${WD}";
-    info "SRC = \t\t ${SRC}";
-    info "BUILD = \t ${BUILD}";
-    info "ENGINE_DIR = \t ${ENGINE_DIR}";
-    info "APP_PATH = \t ${APP_PATH}";
-    info "WEB_USER = \t ${WEB_USER}";
-    info "OPTS = \t ${OPTS}";
+  info "CODE_VERSION =  ${CODE_VERSION}";
+  info "GIT_COMMIT = \t ${GIT_COMMIT}";
+  info "WD = \t\t ${WD}";
+  info "SRC = \t\t ${SRC}";
+  info "BUILD = \t ${BUILD}";
+  info "ENGINE_DIR = \t ${ENGINE_DIR}";
+  info "APP_PATH = \t ${APP_PATH}";
+  info "WEB_USER = \t ${WEB_USER}";
+  info "OPTS = \t ${OPTS}";
 
-    splash "$FUNCNAME Finished";
-    return 0;
+  splash "$FUNCNAME Finished";
+  return 0;
 }
 
 ##  ------------------------------------------------------------------------  ##
@@ -122,15 +125,15 @@ function logEnv () {
 ##  ------------------------------------------------------------------------  ##
 
 function preSetupChecks () {
-    splash "$FUNCNAME Started with: (${@})";
+  splash "$FUNCNAME Started with: (${@})";
 
-    okNode
-    okNpm
-    okBower
-    okGulp
+  okNode
+  okNpm
+  okBower
+  okGulp
 
-    splash "$FUNCNAME Finished";
-    return 0;
+  splash "$FUNCNAME Finished";
+  return 0;
 }
 
 
@@ -139,83 +142,82 @@ function preSetupChecks () {
 ##  ------------------------------------------------------------------------  ##
 
 function depsChecks () {
-    splash "$FUNCNAME Started with: (${@})";
+  splash "$FUNCNAME Started with: (${@})";
 
-    check_composer
-    Delay
+  check_composer
+  Delay
 
-    check_engine
-    Delay
+  check_engine
+  Delay
 
-    # fix_permissions
-    # sleep 1;
+  # fix_permissions
+  # sleep 1;
 
-    deps_install
-    Delay
+  deps_install
+  Delay
 
-    # deps_outdated
-    # sleep 1;
+  # deps_outdated
+  # sleep 1;
 
-    splash "$FUNCNAME Finished";
-    # exit 0;
+  splash "$FUNCNAME Finished";
+  # exit 0;
 }
 
 
 function Build () {
-    splash "$FUNCNAME Started with: (${@})";
+  splash "$FUNCNAME Started with: (${@})";
 
-    # mkdir -p ${BUILD} # && chmod 775 ${BUILD}
-    mkdir -p ${BUILD}
-    set_permissions ${BUILD}
+  # mkdir -p ${BUILD} # && chmod 775 ${BUILD}
+  mkdir -p ${BUILD}
+  set_permissions ${BUILD}
 
-    cd ${WD}
-    node gulpfile.js --env=${APP_ENV} #--verbose
-    Delay
+  cd ${WD}
+  node gulpfile.js --env=${APP_ENV} #--verbose
+  Delay
 
-    cd ${WD}
-    mv -p "${BUILD}/.env" "${BUILD}/.env.${DATE}" 2>/dev/null
-    cp -pr ./setup.rc "${BUILD}/.env.setup"
-    cp -pr "${SRC}/composer.json" "${BUILD}/"
-    Delay
+  cd ${WD}
+  mv -p "${BUILD}/.env" "${BUILD}/.env.${DATE}" 2>/dev/null
+  cp -pr ./setup.rc "${BUILD}/.env.setup"
+  cp -pr "${SRC}/composer.json" "${BUILD}/"
+  Delay
 
-    cd ${BUILD}
-    composer -v update
-    Delay
+  cd ${BUILD}
+  composer -v update
+  Delay
 
-    cd ${WD}
-    # sudo chown -R ${WEB_USER}:${WEB_USER} "${BUILD}/"
-    set_permissions ${BUILD}
-    Delay
+  cd ${WD}
+  set_permissions ${BUILD}
+  Delay
 
-    splash "$FUNCNAME Finished";
-    # exit 0;
+  splash "$FUNCNAME Finished";
+  # exit 0;
 }
 
 
 function Deploy () {
-    splash "$FUNCNAME params: (${@})";
+  splash "$FUNCNAME params: (${@})";
 
-    mkdir -p "${WD}/${APP_DIR}/public/" #&& chmod -R 775 "${WD}/${APP_DIR}/public/"
-    set_permissions ${WD}/${APP_DIR}
+  mkdir -p "${WD}/${APP_DIR}/public/";
+  set_permissions ${WD}/${APP_DIR};
 
-    cd ${WD}
-    node gulpfile.js sync:web --env=${APP_ENV} --verbose    \
- && Delay
+  cd ${WD}
+  node gulpfile.js sync:web --env=${APP_ENV} --verbose        \
+  && Delay;
 
-    cd ${WD}
-    cd "${WD}/${APP_DIR}/public/"
-    ln -s ../storage/media/audio/ >&2 2>/dev/null    \
- && Delay
+  cd ${WD}
+  cd "${WD}/${APP_DIR}/public/"
+  ln -s ../storage/media/audio/ >&2 2>/dev/null               \
+  && Delay;
 
-    cd ${WD}
-    sudo chown -R ${WEB_USER}:${WEB_USER} "${APP_DIR}"    \
- && Delay
+  cd ${WD}
+  sudo chown -R ${WEB_USER}:${WEB_USER} "${APP_DIR}"          \
+  && Delay;
 
-    node gulpfile.js artisan:clear --env=${APP_ENV} --verbose    \
- && Delay
+  node gulpfile.js artisan:clear --env=${APP_ENV} --verbose   \
+  && Delay;
 
-    splash "$FUNCNAME Finished";
-    # exit 0;
+  splash "$FUNCNAME Finished";
+  # exit 0;
 }
 
 
@@ -228,63 +230,63 @@ logEnv
 
 case "$1" in
 
-    "")
-        log "without params";
-        usage
-        RETVAL=0
-    ;;
+  "")
+    log "without params";
+    usage
+    RETVAL=0
+  ;;
 
-    "usage" | "h")
-        log "usage()";
-        usage
-        RETVAL=0
-    ;;
+  "usage" | "h")
+    log "usage()";
+    usage
+    RETVAL=0
+  ;;
 
-    "test" | "t")
-        log "test()";
-        preSetupChecks
-        RETVAL=$?
-    ;;
+  "test" | "t")
+    log "test()";
+    preSetupChecks
+    RETVAL=$?
+  ;;
 
-    "prepare" | "prep" | "p")
-        log "prepare()";
-        depsChecks
-        RETVAL=$?
-    ;;
+  "prepare" | "prep" | "p")
+    log "prepare()";
+    depsChecks
+    RETVAL=$?
+  ;;
 
-    "build" | "b")
-        info "build()";
-        Build && Delay
-        RETVAL=$?
-    ;;
+  "build" | "b")
+    info "build()";
+    Build && Delay
+    RETVAL=$?
+  ;;
 
-    "rebuild" | "rb")
-        info "REbuild()";
-        Build && Delay
-        Deploy && Delay
-        RETVAL=$?
-    ;;
+  "rebuild" | "rb")
+    info "REbuild()";
+    Build && Delay
+    Deploy && Delay
+    RETVAL=$?
+  ;;
 
-    "deploy" | "d")
-        info "deploy()";
-        Deploy
-        RETVAL=$?
-    ;;
+  "deploy" | "d")
+    info "deploy()";
+    Deploy
+    RETVAL=$?
+  ;;
 
-    "all" | "a")
-        info "all()";
-        preSetupChecks && Delay
-        depsChecks && Delay
-        Build && Delay
-        Deploy && Delay
-        RETVAL=$?
-    ;;
+  "all" | "a")
+    info "all()";
+    preSetupChecks && Delay
+    depsChecks && Delay
+    Build && Delay
+    Deploy && Delay
+    RETVAL=$?
+  ;;
 
-    *)
-        error "UNKNOWN command: $1";
-        usage
-        RETVAL=1
-    ;;
+  *)
+    error "UNKNOWN command: $1";
+    usage
+    RETVAL=1
+  ;;
 
 esac
 
