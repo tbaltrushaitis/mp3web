97b5b146c4c1815862ff7a8f3cd1a7616c47b851
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2017-11-18 12:35:26 +0200

    :new: function createDirTree()

--- bin/f.sh
+++ bin/f.sh
@@ -23,38 +23,38 @@
 ##  ------------------------------------------------------------------------  ##
 
 function log () {
-    echo -en "\n${White}[$FUNCNAME][$(date +'%Y%m%d%H%M%S')]:\t" "${@}" "\n${NC}";
+  echo -ne "${White}[$FUNCNAME][$(date +'%Y%m%d%H%M%S')]:\t" "${@}" "${NC}\n";
 }
 
 
 function info () {
-    echo -en "\n${BBlue}[$FUNCNAME][$(date +'%Y%m%d%H%M%S')]:\t" "${@}${NC}";
+  echo -ne "${BBlue}[$FUNCNAME][$(date +'%Y%m%d%H%M%S')]:\t" "${@}${NC}\n";
 }
 
 
 function warn () {
-    echo -en "\n${BYellow}[$FUNCNAME][$(date +'%Y%m%d%H%M%S')]:\t" "${@}${NC}";
+  echo -ne "${BYellow}[$FUNCNAME][$(date +'%Y%m%d%H%M%S')]:\t" "${@}${NC}\n";
 }
 
 
 function error () {
-    echo -en "\n${BRed}[$FUNCNAME][$(date +'%Y%m%d%H%M%S')]:\t" "${@}" "\n${NC}" 1>&2;
+  echo -ne "${BRed}[$FUNCNAME][$(date +'%Y%m%d%H%M%S')]:\t" "${@}" "${NC}\n" 1>&2;
 }
 
 
 function fatal () {
-    echo -e "\n${BRed}";
-    echo -ne "****************** $FUNCNAME *************************"
-    echo -ne "%s\n" "$@"
-    echo -ne "****************** $FUNCNAME *************************\n${NC}"
+  echo -e "\n${BRed}";
+  echo -ne "****************** $FUNCNAME *************************"
+  echo -ne "%s\n" "$@"
+  echo -ne "****************** $FUNCNAME *************************${NC}\n"
 }
 
 
 function splash () {
-    echo -ne "\n${BCyan}";
-    echo -ne "****************** === *************************\n"
-    echo -ne "[$FUNCNAME][$(date +'%Y%m%d%H%M%S')]:\t" "${@}" "\n";
-    echo -ne "****************** === *************************\n${NC}"
+  echo -ne "\n${BCyan}";
+  echo -ne "****************** === *************************\n"
+  echo -ne "[$FUNCNAME][$(date +'%Y%m%d%H%M%S')]:\t" "${@}" "\n";
+  echo -ne "****************** === *************************${NC}\n"
 }
 
 ##  ------------------------------------------------------------------------  ##
@@ -62,11 +62,10 @@ function splash () {
 ##  ------------------------------------------------------------------------  ##
 
 function Delay () {
-    local T=1;
-    printf "\nTimeout ${T} second(s) ... ";
-    sleep ${T};
-    printf "DONE\n";
-    # exit 0;
+  local T=1;
+  printf "\nTimeout ${T} second(s) ... ";
+  sleep ${T};
+  printf "DONE\n";
 }
 
 ##  ------------------------------------------------------------------------  ##
@@ -75,75 +74,90 @@ function Delay () {
 
 function loadEnv () {
 
-    local ENVD=$1
-    ENVD=${ENVD:-.}
-    ENV_LIST=$(ls ${ENVD}/.env.*)
-    echo -ne "\n${BWhite}ENV: ENV_LIST = [${ENV_LIST}]${NC}";
+  local ENVD=$1
+  ENVD=${ENVD:-.}
+  ENV_LIST=$(ls ${ENVD}/.env.*)
+  echo -ne "\n${BWhite}ENV: ENV_LIST = [${ENV_LIST}]${NC}";
 
-    if [ ! "${ENV_LIST}" == "" ]; then
-        for ENVF in ${ENV_LIST};
-            do
-                if [ -f "${ENVF}" ]; then
-                    . "${ENVF}";
-                    echo -ne "\n${BWhite}ENV: exported vars from [${ENVF}]:\n";
-                    cat "${ENVF}" | sed -e "s/^/\t/g"
-                fi
-            done
-    fi
+  if [ ! "${ENV_LIST}" == "" ]; then
+    for ENVF in ${ENV_LIST};
+      do
+        if [ -f "${ENVF}" ]; then
+          . "${ENVF}";
+          echo -ne "\n${BWhite}ENV: exported vars from [${ENVF}]:\n";
+          cat "${ENVF}" | sed -e "s/^/\t/g"
+        fi
+      done
+  fi
 
 }
 
 
 function saveEnv () {
 
-    local ENVD=$1
-    local ENVC=$2
-    ENVD=${ENVD:-.}
-    ENVC=${ENVC:-.env}
-    local ENVDC=${ENVD}/${ENVC}
+  local ENVD=$1
+  local ENVC=$2
+  ENVD=${ENVD:-.}
+  ENVC=${ENVC:-.env}
+  local ENVDC=${ENVD}/${ENVC}
 
-    ENV_LIST=$(ls ${ENVD}/.env.*)
-    echo -ne "\n${BWhite}ENV \t ENV_LIST = [${ENV_LIST}]${NC}\n";
+  ENV_LIST=$(ls ${ENVD}/.env.*)
+  echo -ne "\n${BWhite}ENV \t ENV_LIST = [${ENV_LIST}]${NC}\n";
 
-    if [ ! "${ENV_LIST}" == "" ]; then
+  if [ ! "${ENV_LIST}" == "" ]; then
 
-        if [ -f "${ENVDC}" ]; then
-            # mv -T "${ENVDC}" "${ENVDC}.$(date +"%s").bak"
-            rm -f "${ENVDC}"
-        fi
-
-        # for ENVF in `ls ${ENVD}/.env.*`;
-        for ENVF in ${ENV_LIST};
-            do
-                if [ -f "${ENVF}" ]; then
-                    . "${ENVF}";
-                    echo -ne "## Vars from [${ENVF}]\n" >> "${ENVDC}"
-                    # cat "${ENVF}" >> "${ENVDC}"
-                    # cat "${ENVF}" | sed -e '/^#/d' >> "${ENVDC}"
-                    sed -e '/^#/d' "${ENVF}" >> "${ENVDC}"
-                    echo -ne "\n" >> "${ENVDC}"
-                fi
-            done
+    if [ -f "${ENVDC}" ]; then
+      # mv -T "${ENVDC}" "${ENVDC}.$(date +"%s").bak"
+      rm -f "${ENVDC}"
     fi
 
+    # for ENVF in `ls ${ENVD}/.env.*`;
+    for ENVF in ${ENV_LIST};
+      do
+        if [ -f "${ENVF}" ]; then
+          . "${ENVF}";
+          echo -ne "## Vars from [${ENVF}]\n" >> "${ENVDC}"
+          # cat "${ENVF}" >> "${ENVDC}"
+          # cat "${ENVF}" | sed -e '/^#/d' >> "${ENVDC}"
+          sed -e '/^#/d' "${ENVF}" >> "${ENVDC}"
+          echo -ne "\n" >> "${ENVDC}"
+        fi
+      done
+  fi
+
 }
 
 
 ##  ------------------------------------------------------------------------  ##
-##                              PERMISSIONS
+##                                FILESYSTEM                                  ##
 ##  ------------------------------------------------------------------------  ##
+
+function createDirTree {
+  local TREE_LIST="$1";
+  printf "\n--------------------  CREATE PROJECT DIRS  ---------------------\n";
+  info "TREE_LIST = [${TREE_LIST}]";
+
+  for D in "${TREE_LIST}"
+    do
+      info "D = [${D}]";
+      mkdir -p "${D}" 2>&1 > /dev/null;
+    done
+
+  printf "CREATED PROJECT DIRS FROM LIST: [${W_DIR}] \n\n";
+  printf "==================================================================\n";
+}
+
 function set_permissions {
-    W_DIR="$1"
-    printf "\n------------------  SET PERMISSIONS  ------------------------\n";
-    info "W_DIR = ${W_DIR}"
-    sudo chown -R root:"${WEB_USER}" "${W_DIR}"
-    sudo chmod 775 "${W_DIR}"
+  W_DIR="$1"
+  printf "\n------------------------  SET PERMISSIONS  ---------------------\n";
+  info "W_DIR = ${W_DIR}"
+  sudo chown -R ${WEB_USER}:${WEB_USER} "${W_DIR}"
+  sudo chmod 775 "${W_DIR}"
 
-    cd "${W_DIR}"
-    sudo find . -type d -exec chmod 775 {} \;
-    sudo find . -type f -exec chmod 664 {} \;
+  cd "${W_DIR}"
+  sudo find . -type d -exec chmod 775 {} \;
+  sudo find . -type f -exec chmod 664 {} \;
 
-    printf "PERMISSIONS CHANGED FOR: [${W_DIR}] \n\n";
-    printf "======================================================\n";
+  printf "PERMISSIONS CHANGED FOR: [${W_DIR}] \n\n";
+  printf "==================================================================\n";
 }
-
