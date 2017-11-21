7711c340f8c7399085c3093240bdbf1c4992abf4
Baltrushaitis Tomas <tbaltrushaitis@gmail.com>
2017-11-21 11:24:48 +0200

    corrected variables substitution

--- bin/f-engine.sh
+++ bin/f-engine.sh
@@ -12,8 +12,8 @@
 
 
 function engine_check {
-  if [ ! -d "${ENGINE_DIR}" ]; then
-    error "Engine directory [${ENGINE_DIR}] not found!";
+  if [ ! -d "${DIR_ENGINE}" ]; then
+    error "Engine directory [${DIR_ENGINE}] not found!";
     warn "Starting engine setup ... \n";
     engine_setup
     fix_engine_permissions
@@ -24,8 +24,8 @@ function engine_check {
 
 function engine_setup {
   # composer -vvv create-project --prefer-dist ${ENGINE_NAME}/${ENGINE_TAG} "${ENGINE_DIR}" "${ENGINE_VERSION}.*"
-  git clone -b "v${ENGINE_VERSION}" "https://github.com/laravel/laravel" "${ENGINE_DIR}"
-  warn "Engine ${ENGINE_NAME}-${ENGINE_TAG} DEPLOYED to [${ENGINE_DIR}]";
+  git clone -b "v${ENGINE_VERSION}" "https://github.com/laravel/laravel" ${DIR_ENGINE}
+  warn "Engine ${ENGINE_NAME}-${ENGINE_TAG} CLONED to [${DIR_ENGINE}]";
   # cp -pr ${ENGINE_DIR}/* build/
   # warn "Engine directory [${ENGINE_DIR}] COPIED to [build/]";
   # cp -prv setup.rc build/.env
@@ -35,11 +35,11 @@ function engine_setup {
 
 
 function fix_engine_permissions {
-  cd "${APP_HOME}"
-  sudo chown -R ${WEB_USER}:${WEB_USER} ${ENGINE_DIR}
-  sudo chmod 775 "${ENGINE_DIR}"
-  sudo chmod 775 "${ENGINE_DIR}/storage"
-  sudo chmod 775 "${ENGINE_DIR}/bootstrap/cache"
+  cd ${APP_HOME}
+  # sudo chown -R ${WEB_USER}:${WEB_USER} ${DIR_ENGINE}
+  sudo chmod 775 ${DIR_ENGINE}
+  sudo chmod 775 ${DIR_ENGINE}/storage
+  sudo chmod 775 ${DIR_ENGINE}/bootstrap/cache
 }
 
 ##  -----------------------  EOF: f-engine.sh  -----------------------------  ##
